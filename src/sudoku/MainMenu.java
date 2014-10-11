98219dca0071eadbd94d50b3f152f90fcbd3f846
Hollingsworth-Miquelyn <miquelyn10@gmail.com>
2014-10-11 14:56:12 -0600

    commit

--- src/sudoku/MainMenu.java
+++ src/sudoku/MainMenu.java
@@ -6,21 +6,41 @@
 
 package sudoku;
 
+import java.util.Scanner;
 /**
  *
  * @author Miquelyn
  */
 public class MainMenu {
-    private String help = "The instructions will go here.";
-    private String exit = "Are you sure you want to exit the game?";
+      
+    public void displayMenu(){
+        System.out.println("1) New Game\n2) Help\n3) Exit");
+        System.out.println("Select: ");
+    }
     
     public MainMenu (){
-    }
-    
-    public void displayInstructions(){
-        System.out.println(this.help);
-    }
-    public void displayExit(){
-        System.out.println(this.exit);
+        Scanner in = new Scanner (System.in);
+        
+        displayMenu();
+        switch (in.nextInt()){
+            case 1:
+                System.out.println("You selected New Game");//need to take user to new game menu options
+                break;
+            case 2:
+                System.out.println("********************************************************\n"
+                        + "********************INSTRUCTIONS************************\n"
+                        + "Fill the empty cells with the numbers 1-9.\n" 
+                        + "Each number can appear only once in each column and row.\n"
+                        + "Each number can appear only once in each 3x3 sub-square.\n"
+                        +"********************************************************\n"
+                        +"********************************************************\n");
+                break;
+            case 3:
+                System.out.println("Are you sure you want to exit the game?");
+                break;
+            default:
+                System.err.println("Unrecognized option");
+                break;
+        }
     }
 }
