68b265b365706b9a7d0b56b20450cb69302e0571
Hollingsworth-Miquelyn <miquelyn10@gmail.com>
2014-10-15 17:49:20 -0600

    commit

---
+++ src/sudoku/MainMenuControl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+/**
+ *
+ * @author Miquelyn Hollingsworth
+ */
+public class MainMenuControl {
+    
+public void newGame() {
+        System.out.println();        
+        System.out.println( 
+                "\tThis will start the New Game Menu"
+                );
+    }        
+          
+    public void displayHelpMenu() {
+        HelpMenuView helpMenu = new HelpMenuView();
+        helpMenu.getInput();
+    }
+    
+}
+
