867c839c2ad13f063880b0d317f6b5c54ffa384a
H Spackman <hcspack@gmail.com>
2014-11-22 23:08:44 -0700

    Heidi Lesson 9 individual assignment

---
+++ src/sudoku/ChristmasLesson9.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+/**
+ *Lesson 9
+ * @author Heidi Spackman
+ */
+/*public class ChristmasLesson9 {
+    double temperature;
+    double gift;
+    
+    void showDim () {
+        System.out.println("Temperature is: " + temperature + "and number of gifts equals: " + gift);
+    }
+    
+}
+//subclass
+class Santa extends ChristmasLesson9{
+    String color;
+    
+    double whatcolor() {
+        return temperature * 1;
+    }
+
+    void showStyle() {
+        System.out.println("Santa is wearing " + color + " this year!");
+    }
+}
+
+class Christmas {
+    public static void main (String args[]){
+    ChristmasLesson9 c5 = new ChristmasLesson9();
+    ChristmasLesson9 c9 = new ChristmasLesson9();
+    
+    c5.temperature = 34;
+    c5.gift = 10;
+    c5.color = "red";
+    
+    c9.temperature = 80;
+    c9.gift = 5;
+    c9.color = "white & red";
+    
+    c5.whatcolor();
+    c5.showStyle();
+    System.out.println("Christmas is " + c5.temperature + " degrees this year!");
+    System.out.println("You may only expect " + c5.gift + "gifts this year!");
+    c5.showDim();
+    
+    c9.whatcolor();
+    c9.showStyle();
+    System.out.println("Christmas is " + c9.temperature + " degrees this year!");
+    System.out.println("You may only expect " + c5.gift + "gifts this year!");
+    c9.showDim();
+    }
+}*/
