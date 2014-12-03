0cce729d421c671196813c7a832ad114120069b5
H Spackman <hcspack@gmail.com>
2014-12-03 11:26:20 -0700

    Heidi Spackman Lesson 11 Individual Assignment

---
+++ src/exception/GameException.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception;
+
+/**
+ *
+ * @author Heidi
+ */
+public class GameException extends Exception {
+    public GameException() {
+    }
+
+    public GameException(String message) {
+        super(message);
+    }
+
+    public GameException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GameException(Throwable cause) {
+        super(cause);
+    } 
+}
